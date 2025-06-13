65ef120e8c919356978c63d6c059ca1bb82ff587
João Vitor <joao_monteiro@discente.ufg.br>
2025-06-13 10:21:52 -0300

    Deu errado
    o Caça Niquel ta iniciando sozinho antes do login e senha e quando chamado a funcao nao da o resultado da aposta

--- src/core/menu.py
+++ src/core/menu.py
@@ -1,4 +1,8 @@
 from games import roleta
+<<<<<<< Updated upstream
+=======
+from games import cacaNiqueis
+>>>>>>> Stashed changes
 
 UserId = input('Login: ')
 input('Senha: ')
@@ -7,10 +11,16 @@ conta = 10
 
 print('ROLETA ONLINE')
 print('Conta: R$ :' + str(conta))
-op = int(input('1. Jogar\n2. Adicionar fundos\n'))
+op = int(input('1. Jogar na Roleta\n2. Jogar no Caça níquel\n3. Adicionar fundos\n'))
 if op==1:
     roleta.Game(conta)
+<<<<<<< Updated upstream
 if op==2:
+=======
+if op == 2:
+    cacaNiqueis.Nique(conta)
+if op==3:
+>>>>>>> Stashed changes
     print('Adicionar fundos:\n')
     add = int(input('Valor: '))
     conta = conta + add
